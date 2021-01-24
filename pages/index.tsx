67aa3263bcf7a8bb5dba9bab8c827e52d6dc1e5f
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-01-24 14:33:34 +0900

    fix css

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,53 +26,55 @@ export default function Home({ markdown }) {
       </Head>
       <main>
         <div className="relative h-screen w-screen">
-          <div className="absolute inset-0 ml-auto w-24 h-4">
+          <div className="absolute inset-0 ml-auto w-24 h-4 z-10">
             <div className="m-4 hover:underline">
               <Link href="/jp">
                 <a>日本語</a>
               </Link>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row items-center absolute inset-0 m-auto w-1/2 h-1/2">
-            <div className="m-4">
-              <Image
-                src="/me.jpg"
-                alt="my pic"
-                width={200}
-                height={200}
-                layout="fixed"
-                className="rounded-full"
-              />
-            </div>
-            <div className="max-w-xl ">
-              <div className="text-3xl mb-4">Rintaro Hasegawa</div>
-              <div className="text-l mb-4">
-                I'm a Software Enginner, and CTO of IZA k.k. in Tokyo. I was
-                born in Sapporo, Japan, and graduated the University of Tokyo in
-                System Engineering. I run 100km every month.
+          <div className="absolute inset-0 m-auto w-screen h-screen flex">
+            <div className="m-auto flex flex-col md:flex-row items-center">
+              <div className="m-4">
+                <Image
+                  src="/me.jpg"
+                  alt="my pic"
+                  width={200}
+                  height={200}
+                  layout="fixed"
+                  className="rounded-full"
+                />
               </div>
-              <div className="flex">
-                <div className="flex items-center mr-4">
-                  <FaGithubAlt />
-                  <span className="mr-1" />
-                  <A href="https://github.com/h13o">Github</A>
+              <div className="max-w-xl ml-2">
+                <div className="text-3xl mb-4">Rintaro Hasegawa</div>
+                <div className="text-l mb-4">
+                  I'm a Software Enginner, and CTO of IZA k.k. in Tokyo. I was
+                  born in Sapporo, Japan, and graduated the University of Tokyo
+                  in System Engineering. I run 100km every month.
                 </div>
-                <div className="flex items-center mr-4">
-                  <FaLinkedin />
-                  <span className="mr-1" />
-                  <A href="https://www.linkedin.com/in/rintaro-hasegawa-b9b465143/">
-                    Linkedin
-                  </A>
-                </div>
-                <div className="flex items-center mr-4">
-                  <FaFacebook />
-                  <span className="mr-1" />
-                  <A href="https://www.facebook.com/DOZAEMORIN/">Facebook</A>
-                </div>
-                <div className="flex items-center">
-                  <FaTwitter />
-                  <span className="mr-1" />
-                  <A href="https://twitter.com/_h13o">Twitter</A>
+                <div className="flex flex-col md:flex-row">
+                  <div className="flex items-center mr-4">
+                    <FaGithubAlt />
+                    <span className="mr-1" />
+                    <A href="https://github.com/h13o">Github</A>
+                  </div>
+                  <div className="flex items-center mr-4">
+                    <FaLinkedin />
+                    <span className="mr-1" />
+                    <A href="https://www.linkedin.com/in/rintaro-hasegawa-b9b465143/">
+                      Linkedin
+                    </A>
+                  </div>
+                  <div className="flex items-center mr-4">
+                    <FaFacebook />
+                    <span className="mr-1" />
+                    <A href="https://www.facebook.com/DOZAEMORIN/">Facebook</A>
+                  </div>
+                  <div className="flex items-center">
+                    <FaTwitter />
+                    <span className="mr-1" />
+                    <A href="https://twitter.com/_h13o">Twitter</A>
+                  </div>
                 </div>
               </div>
             </div>
--- pages/jp.tsx
+++ pages/jp.tsx
@@ -26,51 +26,53 @@ export default function Home({ markdown }) {
       </Head>
       <main>
         <div className="relative h-screen w-screen">
-          <div className="absolute inset-0 ml-auto w-24 h-4">
+          <div className="absolute inset-0 ml-auto w-24 h-4 z-10">
             <div className="m-4 hover:underline">
               <Link href="/">
                 <a>English</a>
               </Link>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row items-center absolute inset-0 m-auto w-1/2 h-1/2">
-            <div className="m-4">
-              <Image
-                src="/me.jpg"
-                alt="my pic"
-                width={200}
-                height={200}
-                layout="fixed"
-                className="rounded-full"
-              />
-            </div>
-            <div className="max-w-xl ">
-              <div className="text-3xl mb-4">長谷川林太郎</div>
-              <div className="text-l mb-4">
-                札幌生まれ、東京大学工学部システム創成学科卒です。大学の同期と始めた株式会社イザで取締役CTOをしています。毎月100km走っています。
+          <div className="absolute inset-0 m-auto w-screen h-screen flex">
+            <div className="m-auto flex flex-col md:flex-row items-center">
+              <div className="m-2">
+                <Image
+                  src="/me.jpg"
+                  alt="my pic"
+                  width={200}
+                  height={200}
+                  layout="fixed"
+                  className="rounded-full"
+                />
               </div>
-              <div className="flex">
-                <div className="flex items-center mr-4">
-                  <FaGithubAlt />
-                  <span className="mr-1" />
-                  <A href="https://github.com/h13o">Github</A>
+              <div className="max-w-xl m-4">
+                <div className="text-3xl mb-4">長谷川林太郎</div>
+                <div className="text-l mb-4">
+                  札幌生まれ、東京大学工学部システム創成学科卒です。大学の同期と始めた株式会社イザで取締役CTOをしています。毎月100km走っています。
                 </div>
-                <div className="flex items-center mr-4">
-                  <FaLinkedin />
-                  <span className="mr-1" />
-                  <A href="https://www.linkedin.com/in/rintaro-hasegawa-b9b465143/">
-                    Linkedin
-                  </A>
-                </div>
-                <div className="flex items-center mr-4">
-                  <FaFacebook />
-                  <span className="mr-1" />
-                  <A href="https://www.facebook.com/DOZAEMORIN/">Facebook</A>
-                </div>
-                <div className="flex items-center">
-                  <FaTwitter />
-                  <span className="mr-1" />
-                  <A href="https://twitter.com/_h13o">Twitter</A>
+                <div className="flex flex-col md:flex-row">
+                  <div className="flex items-center mr-4">
+                    <FaGithubAlt />
+                    <span className="mr-1" />
+                    <A href="https://github.com/h13o">Github</A>
+                  </div>
+                  <div className="flex items-center mr-4">
+                    <FaLinkedin />
+                    <span className="mr-1" />
+                    <A href="https://www.linkedin.com/in/rintaro-hasegawa-b9b465143/">
+                      Linkedin
+                    </A>
+                  </div>
+                  <div className="flex items-center mr-4">
+                    <FaFacebook />
+                    <span className="mr-1" />
+                    <A href="https://www.facebook.com/DOZAEMORIN/">Facebook</A>
+                  </div>
+                  <div className="flex items-center">
+                    <FaTwitter />
+                    <span className="mr-1" />
+                    <A href="https://twitter.com/_h13o">Twitter</A>
+                  </div>
                 </div>
               </div>
             </div>
