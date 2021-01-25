963a33ff3f686a8b9fe6f8e5330e36367766a218
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-01-25 14:15:59 +0900

    fix(google analytics): fix type error

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -2,9 +2,11 @@ export const GA_TRACKING_ID = "G-X2984Y3HBQ";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 type GTagEvent = {
