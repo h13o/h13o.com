9ef76244d725630beef4fbb1dc36921fb1774df4
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-02-21 22:13:26 +0900

    style(posts): title and date

--- components/layouts/PostLayout.tsx
+++ components/layouts/PostLayout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import Head from 'next/head';
 import Prism from "prismjs";
 import "prismjs/components/prism-jsx.min";
+import "prismjs/plugins/line-highlight/prism-line-highlight.min.js";
 
 
 const PostLayout = ({ meta, children }) => {
@@ -13,9 +14,10 @@ const PostLayout = ({ meta, children }) => {
       <Head>
         <title>{meta.title}</title>
       </Head>
-      <div className="m-4">
-        <div>{meta.title}</div>
-        <div className="prose prose-green lg:prose-lg mx-auto">
+      <div className="m-4 max-w-screen-lg lg:mx-auto">
+        <div className="text-3xl mt-24 ">{meta.title}</div>
+        <div className="mt-2">{meta.date}</div>
+        <div className="mt-12 prose prose-green lg:prose-lg max-w-none">
           {children}
         </div>
       </div>
