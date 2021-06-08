2f94313c636a17ae92df50ec11c9c80abde99c64
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-06-08 17:25:55 +0900

    feat: fix design

--- components/ShuhoList.tsx
+++ components/ShuhoList.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 export default function ShuhoList({ data }) {
     console.log(data);
     return (
-        <div>
+        <div className="pb-16">
             <div className="mb-8">
-                <div className="text-green-900 text-4xl font-light mb-1">
+                <div className="text-gray-900 text-4xl font-light mb-1">
                     Shuho
                 </div>
                 <div>
@@ -16,14 +16,14 @@ export default function ShuhoList({ data }) {
                     article => (
                         article.data.title &&
                         <div>
+                            <div className="text-gray-900 font-light mb-1">
+                                {article.start + "~" + article.end}
+                            </div>
                             <Link href={article.url}>
-                                <div className="cursor-pointer text-2xl font-bold text-green-500 hover:underline">
+                                <div className="cursor-pointer text-2xl text-gray-900 hover:underline">
                                     {article.data.title}
                                 </div>
                             </Link>
-                            <div className="text-green-900 font-light">
-                                {article.start + "~" + article.end}
-                            </div>
                         </div>
                     ))
             }
