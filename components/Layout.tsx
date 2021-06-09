e632ddd550777aab88afe5870390df6059d4637c
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2021-06-09 09:42:51 +0900

    fix: fix design

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ export default function Layout({ children }) {
     return (
         <main>
             <div className="min-h-screen bg-gradient-to-b from-white to-green-50">
-                <div className="max-w-screen-md md:mx-auto mx-8">
+                <div className="max-w-screen-md md:mx-auto px-8">
                     {children}
                 </div>
             </div>
